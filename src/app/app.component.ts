d2b4fd618e748c7a9463904b28d9623ab6851d56
baha bettaieb <baha.1996.bt@gmail.com>
2019-04-13 16:33:06 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { GanttEditorComponent, GanttEditorOptions } from 'ng-gantt';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'gant';
+export class AppComponent implements OnInit {
+  @ViewChild('editor') editor: GanttEditorComponent;
+  public editorOptions: GanttEditorOptions;
+  public data: any;
+  public colors = ['gtaskblue', 'gtaskred', 'gtaskyellow']
+
+  constructor(public fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.data = this.createRandom();
+  }
+
+  createRandom() {
+    const projet = {
+      nom: 'p1',
+      datedeb: '2019-1-1',
+      datefin: '2019-1-30',
+      taches: [
+        {
+          _id: '5cb1dad015ffd018ec1b69a7',
+          datedeb: '2019-04-13',
+          datefin: '2019-04-15',
+          nom: 'tache 1'
+
+        },
+        {
+          _id: '5cb1dad015ffd018ec1b69a8',
+          datedeb: '2019-04-16',
+          datefin: '2019-05-18',
+          nom: 'tache 2'
+
+        }
+      ]
+    }
+
+    return projet.taches.map(tache => {
+      const randomColor = this.colors[Math.floor(Math.random() * this.colors.length)];
+      return {
+        'pID': tache._id,
+        'pName': tache.nom,
+        'pStart': tache.datedeb,
+        'pEnd': tache.datefin,
+        'pClass': randomColor,
+        'pLink': '',
+        'pMile': '',
+        'pRes': '',
+        'pComp': 0,
+        'pGroup': '',
+        'pParent': 0,
+        'pOpen': '',
+        'pDepend': '',
+        'pCaption': '',
+        'pNotes': ''
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgGanttEditorModule } from 'ng-gantt'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TextMaskModule } from 'angular2-text-mask';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,6 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgGanttEditorModule,
     AppRoutingModule
   ],
   providers: [],
